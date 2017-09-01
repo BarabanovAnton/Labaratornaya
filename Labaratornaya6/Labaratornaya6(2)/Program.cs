7dde89451580c3310d25cdfe04c5d263078aa045
DESKTOP-5CA7U2T\vsemp <vsemploxo@gmail.com>
2017-09-01 22:59:31 +0300

    lab6

---
+++ Labaratornaya6/Labaratornaya6(2)/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labaratornaya6_2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char delimiter = ' ';
+            string temp = "";
+            string sas = "sas";
+            Console.Write("Введите строку: ");
+            string str1 = Console.ReadLine();
+            Console.WriteLine("Ваша строка: " + str1);
+
+            String[] str2 = str1.Split(delimiter);
+
+            for (int i = 0; i < str2.Length; i++)
+            {
+                temp = str2[i];
+                if(temp.Contains(sas)) Console.Write("Слова содержащие sas: " + temp + " ");
+            }
+        }
+    }
+}
---
+++ Labaratornaya6/Labaratornaya6/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labaratornaya6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char delimiter = ' ';
+            int max = 0;
+            string temp = "";
+            Console.Write("Введите строку: ");
+            string str1 = Console.ReadLine();
+            Console.WriteLine("Ваша строка: " + str1);
+
+            String[] str2 = str1.Split(delimiter);
+
+            for(int i = 0; i < str2.Length; i++)
+            {
+                if (str2[i].Length >= max)
+                {
+                    max = str2[i].Length;
+                    temp = str2[i];
+                }
+                else
+                {
+                    max = max;
+                }
+            }
+            Console.WriteLine("Самое длинное слово в строке: " + temp);
+        }
+    }
+}
