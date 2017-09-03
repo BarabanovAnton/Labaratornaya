461a4845c15a008a725f4e6f2760228317a6b882
DESKTOP-5CA7U2T\vsemp <vsemploxo@gmail.com>
2017-09-03 12:34:48 +0300

    Повтор ввода при ошибке.

--- Discriminant/Discriminant/Program.cs
+++ Discriminant/Discriminant/Program.cs
@@ -10,52 +10,66 @@
     {
         public static void Main(string[] args)
         {
-            Console.Write("Введите значение a: ");
-            var a = Console.ReadLine();
-            var resultA = double.TryParse(a, out var a1);
-            if (resultA == false)
+                Console.Write("Введите значение a: ");
+                var a = inputDate();
+               /* var resultA = double.TryParse(Console.ReadLine(), out var a);
+                if (resultA == false)
+                {
+                    Console.WriteLine("Ошибка, введите число.");
+                }*/
+
+                Console.Write("Введите значение b: ");
+                var b = inputDate();
+                /*var resultB = double.TryParse(Console.ReadLine(), out var b);
+                if (resultB == false)
+                {
+                    Console.WriteLine("Ошибка, введите число.");
+                }*/
+
+                Console.Write("Введите значение с: ");
+                var c = inputDate();
+                /*var resultC = double.TryParse(Console.ReadLine(), out var c);
+                if (resultC == false)
+                {
+                    Console.WriteLine("Ошибка, введите число.");
+                }*/
+
+                var discr = Pow(b, 2) - 4 * a * c;
+                Console.WriteLine("Дискриминант равен: " + discr);
+
+                if (discr > 0)
+                {
+                    var x1 = (-b - Sqrt(discr)) / (2 * a);
+                    var x2 = (-b + Sqrt(discr)) / (2 * a);
+                    Console.WriteLine("Первый корень равен: " + x1);
+                    Console.WriteLine("Второй корень равен: " + x2);
+                }
+
+                else if (discr == 0)
+                {
+                    var x = -b / (2 * a);
+                    Console.WriteLine("Уравнение имеет один корень: " + x);
+                }
+
+                else
+                {
+                    Console.WriteLine("Уравнение не имеет корней.");
+                }
+        }
+
+        public static double inputDate()
+        {
+            while (true)
             {
-                Console.WriteLine("Ошибка, введите число.");
+                if (double.TryParse(Console.ReadLine(), out var i))
+                {
+                    return i;
+                }
+                else
+                {
+                    Console.Write("Ошибка, введите число: ");
+                }
             }
-
-            Console.Write("Введите значение b: ");
-            var b = Console.ReadLine();
-            var resultB = double.TryParse(b, out var b1);
-            if (resultB == false)
-            {
-                Console.WriteLine("Ошибка, введите число.");
-            }
-
-            Console.Write("Введите значение с: ");
-            var c = Console.ReadLine();
-            var resultC = double.TryParse(c, out var c1);
-            if (resultC == false)
-            {
-                Console.WriteLine("Ошибка, введите число.");
-            }
-
-            var discr = Pow(b1, 2) - 4 * a1 * c1;
-            Console.WriteLine("Дискриминант равен: " + discr);
-
-            if (discr > 0)
-            {
-                var x1 = (-b1 - Sqrt(discr)) / (2 * a1);
-                var x2 = (-b1 + Sqrt(discr)) / (2 * a1);
-                Console.WriteLine("Первый корень равен: " + x1);
-                Console.WriteLine("Второй корень равен: " + x2);
-            }
-
-            else if (discr == 0)
-            {
-                var x = -b1 / (2 * a1);
-                Console.WriteLine("Уравнение имеет один корень: " + x);
-            }
-
-            else
-            {
-                Console.WriteLine("Уравнение не имеет корней.");
-            }
-
         }
     }
 }
