c4d067e03aa87f8e313d34020cf909cc462142fa
DESKTOP-5CA7U2T\vsemp <vsemploxo@gmail.com>
2017-09-19 21:54:34 +0300

    Сделана основа лабораторной.

---
+++ Regex_example/Regex_example/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Regex_example
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var pattern = @"\d{0,4}\+\d{0,4}\=\d{0,6}";
+            var inputExpression = Console.ReadLine();
+
+            if (Regex.IsMatch(inputExpression, pattern))
+            {
+                Console.WriteLine("Вы ввели правильно.");
+            }
+
+            else
+            {
+                Console.WriteLine("Вы ввели неправильно.");
+            }
+        }
+    }
+}
