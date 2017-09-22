632b46f511314847366dab53102634b57a1a82b6
DESKTOP-5CA7U2T\vsemp <vsemploxo@gmail.com>
2017-09-22 11:24:16 +0300

    Упрощение переменной pattern.

--- Regex_example/Regex_example/Program.cs
+++ Regex_example/Regex_example/Program.cs
@@ -2,20 +2,26 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
     using System.Linq;
     using System.Text;
     using System.Text.RegularExpressions;
 
     public class Program
     {
+        [SuppressMessage("StyleCop.CSharp.LayoutRules", "SA1510:ChainedStatementBlocksMustNotBePrecededByBlankLine", Justification = "Reviewed. Suppression is OK here.")]
         public static void Main(string[] args)
         {
             Console.Write("Введите выражение: ");
 
-            var pattern = @"\d+\,?\d*\s*\+?\-?\*?\:?\s*\d+\,?\d*\s*\=\s*\d+\,?\d*";
+            var number = @"\d+\,?\d*";
+            var space = @"\s*";
+            var sign = @"\+?\-?\*?\:?";
+            var equalSign = @"\=";
+            var regex = $"{number}{space}{sign}{space}{number}{space}{equalSign}{space}{number}";
             var inputExpression = Console.ReadLine();
 
-            if (Regex.IsMatch(inputExpression, pattern))
+            if (inputExpression != null && Regex.IsMatch(inputExpression, regex))
             {
                 Console.WriteLine("Вы ввели правильно.");
             }
