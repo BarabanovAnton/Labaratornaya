1781fbe7d2f0402967764f1447cb28dfb483cf77
DESKTOP-5CA7U2T\vsemp <vsemploxo@gmail.com>
2017-09-30 16:22:53 +0300

    Исправлена ошибка со знаками.

--- Regex_example/Regex_example/Program.cs
+++ Regex_example/Regex_example/Program.cs
@@ -9,14 +9,13 @@
 
     public class Program
     {
-        [SuppressMessage("StyleCop.CSharp.LayoutRules", "SA1510:ChainedStatementBlocksMustNotBePrecededByBlankLine", Justification = "Reviewed. Suppression is OK here.")]
         public static void Main(string[] args)
         {
             Console.Write("Введите выражение: ");
 
             var number = @"\d+\,?\d*";
             var space = @"\s*";
-            var sign = @"\+?\-?\*?\:?";
+            var sign = @"\+\-\*\:";
             var equalSign = @"\=";
             var regex = $"{number}{space}{sign}{space}{number}{space}{equalSign}{space}{number}";
             var inputExpression = Console.ReadLine();
